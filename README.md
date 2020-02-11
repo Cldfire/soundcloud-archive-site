1f74f5f174e9ada9bfbb82dd88eae7b02c90fc7c
Jarek Samic <cldfire3@gmail.com>
2020-02-11 12:44:34 -0500

    Set up database connection

--- README.md
+++ README.md
@@ -22,6 +22,27 @@ The backend is in `backend` and is written in Rust. It requires Rust nightly due
 rustup set override nightly
 ```
 
+You will need to set up a local [PostgreSQL](https://www.postgresql.org/) server for the backend to connect to. Connection info (port, host, user, database name) are provided through environment variables.
+
+* `POSTGRES_PORT` specifies the database port
+* `POSTGRES_USER` specifies the database user
+* `POSTGRES_DBNAME` specifies the particular database name to connect to
+* `POSTGRES_HOST` specifies host (should be localhost)
+
+You will also need to provide a value for the environment variable `ARGON_SECRET_KEY` that is used for password hashing. You can get a suitable value by doing something like `openssl rand -base64 32` (although for development purposes it doesn't really matter).
+
+All of these environment variables can be provided in a **`.env` file**. Create a file named `.env` in the `soundcloud-archive-site` directory with the following:
+
+```
+POSTGRES_PORT="..."
+POSTGRES_USER="..."
+POSTGRES_DBNAME="..."
+POSTGRES_HOST="..."
+ARGON_SECRET_KEY="..."
+```
+
+See [dotenv](https://github.com/dotenv-rs/dotenv) for more.
+
 After that's done:
 
 * `cargo run` to run the webserver
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,13 +4,27 @@
 use rocket_contrib::{json::Json, serve::StaticFiles};
 use rocket::response::NamedFile;
 use scas_json::AuthCredentials;
+use dotenv::dotenv;
+use postgres::{Client, NoTls};
 
 use std::path::Path;
+use std::sync::Mutex;
+use std::env;
+
+type DbClient = Mutex<Client>;
+struct ArgonSecretKey(String);
 
 /// The error type used throughout the binary
 #[derive(Debug)]
 enum Error {
-    IoErr(std::io::Error)
+    IoErr(std::io::Error),
+    PostgresErr(postgres::error::Error)
+}
+
+impl From<postgres::error::Error> for Error {
+    fn from(err: postgres::error::Error) -> Self {
+        Self::PostgresErr(err)
+    }
 }
 
 /// A "catch-all" to redirect path requests to the index since we are building a SPA
@@ -32,8 +46,18 @@ fn auth_creds(auth_creds: Json<AuthCredentials>) -> Result<(), Error> {
 
 /// Create a Rocket instance.
 fn rocket() -> Result<rocket::Rocket, Error> {
+    dotenv().ok();
+    let client = Client::configure()
+        .port(env::var("POSTGRES_PORT").unwrap().parse().unwrap())
+        .user(&env::var("POSTGRES_USER").unwrap())
+        .dbname(&env::var("POSTGRES_DBNAME").unwrap())
+        .host(&env::var("POSTGRES_HOST").unwrap())
+        .connect(NoTls)?;
+
     Ok(
         rocket::ignite()
+            .manage(Mutex::new(client))
+            .manage(ArgonSecretKey(env::var("ARGON_SECRET_KEY").unwrap()))
             // TODO: if the directory you put frontend static files in is different
             // then change this accordingly
             .mount("/", StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/frontend/public")))
