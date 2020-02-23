55339a2320c7bb070358a9aaa0a7395bdfcdafa1
ian <icp3@zips.uakron.edu>
2020-02-23 18:45:42 -0500

    Add enum, html class, output fn

--- frontend/Graphics.js
+++ frontend/Graphics.js
@@ -1,5 +1,11 @@
 var body;
+var head;
+var html;
 
 function initialize() {
+    html = document.getElementById("html");
+    head = document.getElementById("head");
     body = document.getElementById("body");
+
+
 }
