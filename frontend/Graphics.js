7a840519196cd72bd84986b764ba4e9a8bca98a8
ian <icp3@zips.uakron.edu>
2020-02-27 08:32:04 -0500

    Transitioning laptop

--- frontend/Graphics.js
+++ frontend/Graphics.js
@@ -1,11 +1,20 @@
-var body;
-var head;
-var html;
+
+const tags = {
+    html: document.getElementById("html"),
+    head: document.getElementById("head"),
+    body: document.getElementById("body"),
+    Username: document.getElementById("Username"),
+    Password: document.getElementById("Password"),
+};
+
+
 
 function initialize() {
     html = document.getElementById("html");
     head = document.getElementById("head");
     body = document.getElementById("body");
-
-
+    body_set();
 }
+
+
+
---
+++ frontend/Login.js
@@ -0,0 +1,13 @@
+function login(){
+    var Login = document.createElement("div");
+    body.appendChild(UserName());
+    body.appendChild(Password());
+}
+
+function UserName(){
+
+}
+
+function Password(){
+
+}
