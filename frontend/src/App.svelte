3967289a733a70a4f8ce6ca555e3fc2d92f0dbd8
Jarek Samic <cldfire3@gmail.com>
2020-04-12 17:52:41 -0400

    Rename `Index.svelte` to `Dashboard.svelte`

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,7 +2,7 @@
     import { Router, Route } from 'yrv';
     import { onMount } from 'svelte';
 
-    import Index from './Index.svelte';
+    import Dashboard from './Dashboard.svelte';
     import Register from './Register.svelte';
     import Login from './Login.svelte';
     import ScCredentials from  './ScCredentials.svelte';
@@ -24,7 +24,7 @@
 </script>
 
 <Router>
-    <Route exact path="/" component={Index}/>
+    <Route exact path="/" component={Dashboard}/>
     <Route path="/register" component={Register}/>
     <Route path="/login" component={Login}/>
     <Route path="/set-soundcloud-credentials" component={ScCredentials}/>
