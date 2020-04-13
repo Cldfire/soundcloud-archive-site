e56a809aaee44af9dd012cac910d62e21d369a57
ian <icp3@zips.uakron.edu>
2020-04-12 20:27:32 -0400

    add to track table

--- frontend/src/TracksList.svelte
+++ frontend/src/TracksList.svelte
@@ -5,10 +5,44 @@
     export let tracks;
 </script>
 
+<style>
+    table{
+        table-layout: fixed;
+        width: 100%;
+    }
+    th, td {
+        border: 1px solid white;
+        color: white;
+        width: 33%;
+    }
+    td {
+    }
+    h2 {
+        color: white;
+    }
+</style>
+
+
 {#if tracks.length > 0}
     <h2>Liked Tracks:</h2>
 {/if}
-
+<table width="100%">
+    <tr>
+        <th>Title</th>
+        <th>Length(ms)</th>
+        <th>Playback Count</th>
+        <th>Username</th>
+        <th>Date Created</th>
+    </tr>
+</table>
 <VirtualList height="300px" items={tracks} let:item>
-  <p>{item.title}</p>
+    <table>
+        <tr>
+            <th>{item.title}</th>
+            <th>{item.length_ms}</th>
+            <th>{item.playback_count}</th>
+            <th>{item.username}</th>
+            <th>{item.created_at}</th>
+        </tr>
+    </table>
 </VirtualList>
